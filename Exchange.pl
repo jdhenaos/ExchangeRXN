9b4c76ede7ba1ff9a245ef2f2fc7a6abb2392715
Juan Henao <judhenaosa@unal.edu.co>
2015-07-14 22:01:56 -0500

    Complete version

--- Exchange.pl
+++ Exchange.pl
@@ -3,14 +3,25 @@
 use strict;
 use Data::Dumper;
 
+open my $lose, '>>', '.list';
+open my $OUT, '>>', 'ExchangesRXN';
 my @lines = ();
 
 foreach (0..$#ARGV) {
   open my $IN, "< $ARGV[$_]";
   while (my $line = <$IN>) {
-    chomp;
     my @comp = split(/\[/,$line);
     push @lines, $comp[0];
   }
 }
-print $_."\n" foreach (@lines);
+
+print $lose $_."\n" foreach (@lines);
+my @uniques = `sort .list | uniq`;
+
+foreach my $x (@uniques) {
+  chomp($x);
+  print $OUT "\'EX_$x(e)\'\t\'$x exchange\'\t\'$x\[e\] <=> \'\t\'Exchange\'\t\'\'\n";
+}
+
+close $lose;
+close $OUT;

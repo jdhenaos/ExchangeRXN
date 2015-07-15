87184ae8944ae08f1d3c4c20228dd1cc39e459cd
Juan Henao <judhenaosa@unal.edu.co>
2015-07-14 20:01:59 -0500

    New version

--- Exchange.pl
+++ Exchange.pl
@@ -1,26 +1,16 @@
 #!/usr/bin/perl -w
 
 use strict;
+use Data::Dumper;
 
-open my $file, '<', 'RXN.orphanProducts';
-open  my $file2, '<', 'RXN.orphanSubstrates';
-open my $out, '>>', 'RXN.exchange';
-my @rxn;
+my @lines = ();
 
-while (my $RXN = <$file>) {
-  chomp($RXN);
-  my @names = split(/\[/, $RXN);
-  push(@rxn, $names[0]);
+foreach (0..$#ARGV) {
+  open my $IN, "< $ARGV[$_]";
+  while (my $line = <$IN>) {
+    chomp;
+    my @comp = split(/\[/,$line);
+    push @lines, $comp[0];
+  }
 }
-
-while (my $RXN2 = <$file2>) {
-  chomp($RXN2);
-  my @names2 = split(/\[/, $RXN2);
-  push(@rxn, $names2[0]);
-}
-
-@rxn = sort @rxn;
-
-foreach (@rxn) {
-  print "$_\n"
-}
+print $_."\n" foreach (@lines);
